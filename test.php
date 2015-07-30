afd760675b30bba35e26b284dae2b80fc985d2a9
gulfam <gulfam@qualityclix.com>
2015-07-30 17:33:12 +0500

    Gulfam: test file

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
-//error_reporting(1);
+error_reporting(1);
 echo("1");
-//require_once "includes/config.php";
+require_once "includes/config.php";
 echo("2");
 /*$mSQL = "SELECT * FROM bh_items";
 echo("3");
