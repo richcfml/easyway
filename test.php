8e7d9951b7ab33f0a7b781589d0cacdd2e92766b
gulfam <gulfam@qualityclix.com>
2015-07-30 17:50:21 +0500

    Gulfam: test file

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
-require_once "includes/config.php";
+include_once "includes/config.php";
 $mSQL = "SELECT * FROM bh_items";
 $mRes = dbAbstract::Execute($mSQL);
 echo("<pre>");
