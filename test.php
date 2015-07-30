e63eda12ad675713d12ac30cf6964fd9fc8243cb
gulfam <gulfam@qualityclix.com>
2015-07-30 17:02:56 +0500

    Gulfam: test file

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
+echo("1");
 require_once "includes/config.php";
+echo("2");
 $mSQL = "SELECT * FROM bh_items";
+echo("3");
 $mRes = dbAbstract::Execute($mSQL);
-
+echo("4");
 echo("<pre>");
 while ($ar = dbAbstract::returnArray($mRes))
 {
