9c08c9fcd83d006802bac7af2353fed14f0c4d9d
gulfam <gulfam@qualityclix.com>
2015-07-30 17:44:54 +0500

    Gulfam: test file

--- test.php
+++ test.php
@@ -1,18 +1,18 @@
 <?php
 error_reporting(1);
-echo("1");
+
 //require_once "includes/config.php";
 $mysqli = new mysqli("localhost", "easywayordering", "Yh56**ew!d", "upgraded_easywayordering");
-echo("2");
+
 $mSQL = "SELECT * FROM bh_items";
-echo("3");
+
 $mRes = mysqli_query($mysqli, $mSQL);
-echo("4");
-/*
+
+
 echo("<pre>");
 while ($ar = dbAbstract::returnArray($mRes))
 {
     print_r($ar);
 }
-echo("</pre>");*/
+echo("</pre>");
 ?>
