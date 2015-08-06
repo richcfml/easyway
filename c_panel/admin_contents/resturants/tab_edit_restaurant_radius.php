b3dc2d130d4003a7f1d826a8bc21194c23871df2
gulfam <gulfam@qualityclix.com>
2015-08-06 23:24:00 +0500

    Gulfam: 1 file

--- c_panel/admin_contents/resturants/tab_edit_restaurant_radius.php
+++ c_panel/admin_contents/resturants/tab_edit_restaurant_radius.php
@@ -297,10 +297,10 @@ if (isset($_POST['submit'])) {
     {
         $errMessage = "Pleaser Enter Valid Logo";
     } 
-    else if (!empty($_FILES['userfile2']['name']) && $myimage2->ProcessImage() == false) 
+    /*else if (!empty($_FILES['userfile2']['name']) && $myimage2->ProcessImage() == false) 
     {
         $errMessage = "Pleaser Enter Valid Logo Thumbnail";
-    } 
+    }*/ 
     else if ($rest_zip == '') 
     {
         $errMessage = "Please enter resturant zip code";
