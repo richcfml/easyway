cbed83b488053c397580ef8564bfe946fdd95dca
gulfam <gulfam@qualityclix.com>
2015-08-06 23:33:42 +0500

    Gulfam: 1 file

--- test.php
+++ test.php
@@ -1,11 +1,3 @@
 <?php
-require_once "includes/config.php";
-$mSQL = "SELECT * FROM bh_items";
-$mRes = dbAbstract::Execute($mSQL);
-echo("<pre>");
-while ($ar = dbAbstract::returnArray($mRes))
-{
-    print_r($ar);
-}
-echo("</pre>");
+echo($_SERVER['DOCUMENT_ROOT']);
 ?>
