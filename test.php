9de4c2dc031b12944ff48843fd293425e5c18011
gulfam <gulfam@qualityclix.com>
2015-08-06 23:52:35 +0500

    Gulfam: 1 file

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
-<?php
-echo($_SERVER['DOCUMENT_ROOT']);
+<?Php
+if (extension_loaded('gd')) {
+echo "<br>GD support is loaded ";
+}else{
+echo "<br>GD support is NOT loaded ";
+}
+if(function_exists('gd_info')){
+echo "<br>GD function support is available ";
+}else{
+echo "<br>GD function support is NOT available ";
+}
 ?>
