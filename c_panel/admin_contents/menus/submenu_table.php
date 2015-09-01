69e15ac97ca074c156317d4db690e82c39dae5ad
shoaib <shoaib@qualityclix.com>
2015-09-01 15:11:33 +0500

    COMMENTS

--- c_panel/admin_contents/menus/submenu_table.php
+++ c_panel/admin_contents/menus/submenu_table.php
@@ -205,13 +205,15 @@
                                                             
                                                             <!--	Item Title -->
                                                             <div style="margin-left: 55px; font-weight: bold; <?=(($mRowPr['signature_sandwitch_id'] > 0)? 'text-align:left;color:#FFF;width:100%':'')?>" sub_cat="<?=$mRow["cat_id"]?>" class="<?=(($mRowPr["status"] == 0)? 'disable-menu':'enable-menu')?> <?=(($mRowPr['signature_sandwitch_id'] > 0)? 'ss_prodTitle':'')?>">
+                                                              <div style="width:150px">
 															  <?= wordwrap($mRowPr["item_title"], 10, "\n", true); ?>
+                                                              </div>
                                                             </div>
                                                             <span id="lblProductID" sub_cat="<?=$mRow["cat_id"]?>" style="display: none;"><?= $mRowPr["prd_id"] ?></span>
                                                         </td>
                                                         
                                                         <!--	Item Price -->
-                                                        <td style=" font-size: 12px;float:left;margin-left: 20px;" sub_cat="<?=$mRow["cat_id"]?>">
+                                                        <td style=" font-size: 12px;float:left;margin-left: 10px;" sub_cat="<?=$mRow["cat_id"]?>">
                                                           <div sub_cat="<?=$mRow["cat_id"]?>" class="<?=(($mRowPr["status"] == 0)? 'disable-menu':'enable-menu')?>" <?=(($mRowPr['signature_sandwitch_id'] > 0)? 'style="color:#FFF;margin-top: 5px;"':'')?>>
 														  	<?= $currency . $mRowPr["retail_price"] ?>
                                                           </div>
