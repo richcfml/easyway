18275d9924197f28203c9caaae06dd4837037d21
gulfam <gulfam@qualityclix.com>
2015-09-04 15:40:00 +0500

    Gulfam: Test PHP file.

--- test.php
+++ test.php
@@ -1,12 +1,28 @@
-<?Php
-if (extension_loaded('gd')) {
-echo "<br>GD support is loaded ";
-}else{
-echo "<br>GD support is NOT loaded ";
-}
-if(function_exists('gd_info')){
-echo "<br>GD function support is available ";
-}else{
-echo "<br>GD function support is NOT available ";
-}
+<?php 
+    include("includes/config.php");
+    $mRes = dbAbstract::Execute("SELECT start_date, end_date FROM bh_signature_sandwitch LIMIT 1");
+    if ($mRes)
+    {
+        $mRow = dbAbstract::returnObject($mRes);
+        if ($mRow)
+        {
+            $mStartDateDB = $mRes->start_date;
+            $mEndDateDB = $mRes->end_date;
+            echo("Server Timezone: ".date_default_timezone_get());
+            echo("Start Date/Time: ".$mStartDateDB.", ".gmdate("Y-m-d H:i:s", $mStartDateDB));
+            echo("<br />End Date/Time: ".$mEndDB.", ".gmdate("Y-m-d H:i:s", $mEndDB));
+            date_default_timezone_set("Europe/London");
+            $mCurrentTimeStamp = strtotime(date("Y-m-d H:i:s"));
+            echo("Restaurant Timezone: ".date_default_timezone_get());
+            echo("<br /><br /><br />Current Date/Time: ".$mCurrentTimeStamp.", ".gmdate("Y-m-d H:i:s", $mCurrentTimeStamp));
+        }
+        else
+        {
+            echo("No Signature Sandwich");
+        }
+    }
+    else
+    {
+        echo("No Signature Sandwich");
+    }
 ?>
