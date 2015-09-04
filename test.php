b51a4cc5c129f644b50b3c026065da3cfe1905a9
gulfam <gulfam@qualityclix.com>
2015-09-04 15:49:07 +0500

    Gulfam: Test PHP file.

--- test.php
+++ test.php
@@ -1,17 +1,20 @@
 <div style="font-family: Arial; font-size: 14px;">
 <?php 
     include("includes/config.php");
-    $mRes = dbAbstract::Execute("SELECT start_date, end_date FROM bh_signature_sandwitch LIMIT 1");
+    $mRes = dbAbstract::Execute("SELECT start_date, end_date FROM bh_signature_sandwitch");
     if ($mRes)
     {
-        $mRow = dbAbstract::returnObject($mRes);
-        if ($mRow)
+        if (dbAbstract::returnRowsCount($mRes))
         {
-            $mStartDateDB = $mRow->start_date;
-            $mEndDateDB = $mRow->end_date;
             echo("Server Timezone: ".date_default_timezone_get());
-            echo("<br />Start Date/Time: ".$mStartDateDB.", ".gmdate("Y-m-d H:i:s", $mStartDateDB));
-            echo("<br />End Date/Time: ".$mEndDateDB.", ".gmdate("Y-m-d H:i:s", $mEndDateDB));
+            while ($mRow = dbAbstract::returnObject($mRes))
+            {
+                $mStartDateDB = $mRow->start_date;
+                $mEndDateDB = $mRow->end_date;
+                echo("<br />Start Date/Time: ".$mStartDateDB.", ".gmdate("Y-m-d H:i:s", $mStartDateDB));
+                echo("<br />End Date/Time: ".$mEndDateDB.", ".gmdate("Y-m-d H:i:s", $mEndDateDB));
+            }
+            
             date_default_timezone_set("Europe/London");
             $mCurrentTimeStamp = strtotime(date("Y-m-d H:i:s"));
             echo("<br /><br /><br />Restaurant Timezone: ".date_default_timezone_get());
