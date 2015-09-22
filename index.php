c0da19d39e81fa476436142dc4f0b4921c8f08b2
gulfam <gulfam@qualityclix.com>
2015-09-22 17:10:41 +0500

    Gulfam: index.php, 1 file

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+if((!isset($_SERVER['HTTPS'])) || ($_SERVER['HTTPS'] == "") || ($_SERVER['HTTPS'] == "off"))
+{
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: $redirect");
+}
+
 require_once "includes/config.php";
 if(isset($_GET['session_id']))
 {
