0adc219aa885487869009d3b1c21943f0c381365
gulfam <gulfam@qualityclix.com>
2015-09-28 11:14:09 +0500

    Gulfam: satisfaction percentage change, 1 file.

--- bh_api/ewoapi_v1.php
+++ bh_api/ewoapi_v1.php
@@ -875,13 +875,13 @@ if (!(isset($mReturn["errorCode"])))
     }
 }
 
-/*echo("<pre>");
+echo("<pre>");
 print_r($mReturn);
-echo("</pre>");*/
+echo("</pre>");
 
 $json = json_encode($mReturn, true);
 
-echo($json);
+//echo($json);
 /* Search Functions Ends Here */
 
 /* General (Helping) Functions Starts Here */
