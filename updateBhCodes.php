31db61696a90efec79e022e091b9ac55ecb4705e
shoaib <shoaib@qualityclix.com>
2015-10-13 15:34:11 +0500

    COMMENTS

--- updateBhCodes.php
+++ updateBhCodes.php
@@ -30,7 +30,7 @@ while ($row = dbAbstract::returnObject($rs))
 	}
 	
 	if($update){
-		dbAbstract::Update("update product set item_des = '$description' where prd_id=".$row->prd_id);
+		dbAbstract::Update("update product set item_des = '".$description."' where prd_id=".$row->prd_id);
 		echo "Before<br>";
 		echo $row->item_des."<br><br>";
 		
