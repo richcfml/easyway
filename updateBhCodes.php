d26feb70ac2186c472d78a9165391a30cd1fa578
shoaib <shoaib@qualityclix.com>
2015-10-13 16:36:11 +0500

    COMMENTS

--- updateBhCodes.php
+++ updateBhCodes.php
@@ -53,6 +53,7 @@ function replaceBhSpecialChars($pDescription)
 	
 	$pDescription = str_replace("®", "&#174;", $pDescription);
 	$pDescription = str_replace("®", "&#174;", $pDescription);
+	$pDescription = str_replace("®", "&#174;", $pDescription);
 	
     $pDescription = str_replace("ä", "&#228;", $pDescription);
     $pDescription = str_replace("è", "&#232;", $pDescription);
