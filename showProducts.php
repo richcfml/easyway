e9242bbe915f2cb4653989d4ccbd8c6cad0293b8
gulfam <gulfam@qualityclix.com>
2015-10-20 23:32:01 +0500

    Gulfam, showproducts.php

--- showProducts.php
+++ showProducts.php
@@ -4,12 +4,13 @@ set_time_limit(1000);
 ini_set('max_execution_time', 1000);
 require_once "includes/config.php";
 
-$mRes= dbAbstract::Execute("SELECT prd_id, item_des FROM product WHERE LENGTH(item_des) > 0 AND LOWER(item_des) LIKE '%proudly featuring boar%' LIMIT 2000");
+$mRes= dbAbstract::Execute("SELECT prd_id, item_des FROM product WHERE LENGTH(item_des) > 0 AND LOWER(item_des) LIKE '%proudly featuring boar%' ORDER BY UpdatedOn DESC LIMIT 5000");
 $mCount = 0;
 while ($mRow = dbAbstract::returnObject($mRes))
 {
     $mCount = $mCount + 1;
-    echo($mRow->prd_id."<br />".$mRow->item_des."<br /><br /><br />");
+    echo("<b>Product ID: </b>".$mRow->prd_id."<br /><b>Description: </b>".$mRow->item_des);
+    echo("<br />- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br /><br />");
 }
 
 echo("<b><span style='color: red'>Total BH Products:: ".$mCount."</span></b>");
