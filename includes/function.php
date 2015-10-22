b81df8caf8d0a78895f8d871ecb1427d6c1d9a85
gulfam <gulfam@qualityclix.com>
2015-10-22 10:31:53 +0500

    Gulfam: function.php

--- includes/function.php
+++ includes/function.php
@@ -465,7 +465,9 @@ function posttoVCS($orderId, $faxstatus, $faxid)
 		if($rest_rs['phone_notification'] == 1) //If phone notifications are ON then send JSON to phone server.
 		{
 			global $VcsURL;
-    		$this->do_post_request($VcsURL,json_encode ($order),""); //$VcsURL is defined in includes/config.php
+                        Log::write("PostTOVCS: Send to VCS, Before Call", print_r(json_encode($order)), 'send2VCS', 0, '');
+                        $this->do_post_request($VcsURL,json_encode ($order),""); //$VcsURL is defined in includes/config.php
+                        Log::write("PostTOVCS: Send to VCS, Call Sent", print_r(json_encode($order)), 'send2VCS', 0 , '');
 		}
 	}
 	catch (Exception $e)  
