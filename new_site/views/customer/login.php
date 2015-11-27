68f1d5544ce605ac68cd5282e37d5a799913ad1a
gulfam <gulfam@qualityclix.com>
2015-11-27 16:27:50 +0500

    Gulfam: Add to cart delay change.

--- new_site/views/customer/login.php
+++ new_site/views/customer/login.php
@@ -100,7 +100,7 @@ else if (isset($_POST['btnregister']))
     
     $mSalt = hash('sha256', mt_rand(10,1000000));    
     $loggedinuser->salt= $mSalt;
-    $loggedinuser->ePassword= hash('sha256', trim($user_password).$mSalt);
+    $loggedinuser->epassword= hash('sha256', trim($user_password).$mSalt);
     $loggedinuser->cust_your_name = trim($first_name);
     $loggedinuser->LastName = trim($last_name);
     $loggedinuser->street1 = trim($address1);
