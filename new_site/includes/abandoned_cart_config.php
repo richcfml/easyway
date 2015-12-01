aa4197e10e5c92357faf013e6912a05406c31bdb
gulfam <gulfam@qualityclix.com>
2015-12-01 18:30:47 +0500

    Gulfam: Add to cart delay changes.

--- new_site/includes/abandoned_cart_config.php
+++ new_site/includes/abandoned_cart_config.php
@@ -1,14 +1,6 @@
 <?php
-if (isset($_GET["l"]))
-{
-    log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-}
 if($cart->order_created===0) 
 {
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
     if(empty($_SESSION["user_session_duration"])) 
     {
         $_SESSION["user_session_duration"] = time();
@@ -18,10 +10,7 @@ if($cart->order_created===0)
     {
         $_SESSION["referral_source"] = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "/");
     }
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+
     $last_user_action = "";
     if($mod == "product") 
     {
@@ -63,10 +52,7 @@ if($cart->order_created===0)
     {
         $last_user_action = "Register Card Details";
     }
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+	
     if($last_user_action != "") 
     {
         $_SESSION["last_user_action"] = $last_user_action;
@@ -75,10 +61,7 @@ if($cart->order_created===0)
     {
         $_SESSION["last_user_action"] = "";
     }
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+
     $user_id = (isset($loggedinuser->id) && is_numeric($loggedinuser->id) ? $loggedinuser->id : 0);
     $resturant_id = $objRestaurant->id;
     $cartt = addslashes(json_encode($cart));
@@ -92,24 +75,14 @@ if($cart->order_created===0)
     {
         $session_duration_in_seconds = 0;
     }
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+
     $cart_total_amount = $cart->grand_total();
     $platform_used = $platform_used;
     $status = 0;
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+
     $reason = "Unknown";
     if(isset($_SESSION["abandoned_cart_error"])) 
     {
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-        }
         $error = $_SESSION["abandoned_cart_error"];
         if(isset($error["redeemcoupon"])) 
         {
@@ -128,42 +101,27 @@ if($cart->order_created===0)
             $error1 = $error["under_delivery_minimum"];
             $reason = "Zone: " . $error1["zone"] . ". Minimum Total required: $". $error1["minTotal"];
         }
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-        }
     }
 
     $last_user_action = $_SESSION["last_user_action"];
-    if (isset($_GET["l"]))
-    {
-        log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-    }
+
     if(isset($_SESSION["abandoned_cart_id"])) 
     {
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-        }
         $abandoned_carts->updateAbandonedCart($_SESSION["abandoned_cart_id"], $user_id, $resturant_id, $cartt, $date_added, $referral_source, $session_duration_in_seconds,$last_user_action,$cart_total_amount,$reason,$platform_used,$status);
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-        }
     } 
     else 
     {
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
-        }
         if(!$cart->isempty())
         {
+            if (isset($_GET["l"]))
+            {
+                log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
+            }
             $_SESSION["abandoned_cart_id"] = $abandoned_carts->addNewAbandonedCart($user_id, $resturant_id, $cartt, $date_added, $referral_source, $session_duration_in_seconds,$last_user_action,$cart_total_amount,$reason,$platform_used,$status);		
-        }
-        if (isset($_GET["l"]))
-        {
-            log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
+            if (isset($_GET["l"]))
+            {
+                log::write("abandoned_cart_config.php: Log ID = ".$_GET["l"].", Line Number = ".__LINE__, "Server Time: ".time()." || Client Time: ".$_GET["t"], "debug");
+            }
         }
     }
 }
